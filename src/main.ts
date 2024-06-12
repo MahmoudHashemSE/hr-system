c210c3ac8c790ff20f9a1f2efc2e82d8cd522640
Mahmoud Hashem <Mahmoud.Hashem.SE@gmail.com>
2024-06-12 23:53:59 +0300

    Add cookie parser and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(helmet());
+  
+  app.enableCors({ origin: '*', credentials: true });
+
+  const configService = app.get(ConfigService);
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const port = configService.getOrThrow('PORT', { infer: true });
+
+  await app.listen(port as number, '0.0.0.0');
 }
 bootstrap();
